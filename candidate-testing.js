276bf54126ccf15372f4e8d554ed04c717b3bd27
Delaney Green <delaney.rosa1@gmail.com>
2023-10-19 15:47:37 -0500

    completed test of GA1-- all tests passing

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,36 +11,48 @@ let candidateAnswer="";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions=["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers=[];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName=input.question("What's your name: ");
-  console.log(candidateName);
+  console.log(`Hello, ${candidateName}. Ready for a quiz?`);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer=input.question(question+": ");
-  console.log(candidateAnswer);
+  for(let i=0;i<questions.length;i++){
+    console.log("\n"+questions[i]);
+    let candidateAnswer=input.question("My Answer: ");
+    candidateAnswers.push(candidateAnswer);
+  }
+  console.log(candidateAnswers);
+
+  // candidateAnswer=input.question(question+": ");
+  // console.log(candidateAnswer);
 
 }
 
 function gradeQuiz(candidateAnswers) {
+  let score=0;
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if(candidateAnswer==correctAnswer){
-    console.log("Your answer "+ candidateAnswer + " is correct")
-  } else {
-    console.log("Incorrect. The correct answer is: "+correctAnswer);
+  for(let i=0;i<candidateAnswers.length;i++){
+    if (candidateAnswers[i].toLowerCase()===correctAnswers[i].toLowerCase()){
+      score+=1;
+      console.log(`Correct! \n Your answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}\n`)
+    } else {
+      console.log(`Incorrect. \n Your answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}\n`)
+    }
   }
 
 
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade=(score/correctAnswers.length)*100;
+  console.log(score, grade);  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
