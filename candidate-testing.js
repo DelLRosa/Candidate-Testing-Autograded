a2165478b33bc9bea5a4f67dacc399bef7b5611b
Delaney Green <delaney.rosa1@gmail.com>
2023-10-19 15:19:48 -0500

    Candidate Testing Part 1 passing tests 1-6

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question="Who was the first American woman in space? ";
+let correctAnswer="Sally Ride";
+let candidateAnswer="";
 
 
 //TODO: Variables for Part 2
@@ -18,18 +18,25 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName=input.question("What's your name: ");
+  console.log(candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer=input.question(question+": ");
+  console.log(candidateAnswer);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if(candidateAnswer==correctAnswer){
+    console.log("Your answer "+ candidateAnswer + " is correct")
+  } else {
+    console.log("Incorrect. The correct answer is: "+correctAnswer);
+  }
 
 
 
